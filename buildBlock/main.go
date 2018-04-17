48d1c798ccc60ff74a526f27d4201f0132a33d32
Simon Ordish <simon.ordish@masagi.co.uk>
2018-04-17 16:11:57 +0100

    Working with example

--- buildBlock/main.go
+++ buildBlock/main.go
@@ -8,36 +8,26 @@ import (
 )
 
 func main() {
-	// j, _ := readJob("j1")
-	// s, _ := readSubmit("s1")
-	// // fmt.Println(j.toString(), s.toString())
+	j, _ := readJob("j1")
+	s, _ := readSubmit("s1")
+	// fmt.Println(j.toString(), s.toString())
 
-	// cb := buildCoinbase(j.Coinbase1, j.Coinbase2, s.ExtraNonce1, s.ExtraNonce2)
-	// merkleRoot := buildMerkleRootFromCoinbase(cb, j.MerkleBranches)
-	// header := buildBlockHeader(j.Version, j.PreviousBlockHash, merkleRoot, s.NTime, j.Bits, s.Nonce)
-	cb, _ := hex.DecodeString("b82c849c6abcd1ad17f4457333afc45723557348d2dda6974363253223b0f378")
-	merkleBranches := []string{
-		"4ea2296eff2cab120ecaa8ea268f663de7b0129f9132ba02de3bde7d3341431f",
-		"39864fb58564307f69945596a9a8e188c256f09a3d67e17bba888e0ae2a99cba",
-		"df46f060d8e9b986eda616f02a22a8cae6f8ae3f19cf2b1ef2c78d03f7bdbf61",
-		"eb1a02c53bc472a480ef12dc609ca71a0137e5d4e80c1e87b138d8d96d4a37f6",
-	}
-	merkleRoot := buildMerkleRootFromCoinbase(cb, merkleBranches)
+	cb := buildCoinbase(j.Coinbase1, j.Coinbase2, s.ExtraNonce1, s.ExtraNonce2)
+	merkleRoot := buildMerkleRootFromCoinbase(cb, j.MerkleBranches)
+
+	time, _ := hex.DecodeString(s.NTime)
+	reverseBytes(time)
+
+	bits, _ := hex.DecodeString(j.Bits)
+	reverseBytes(bits)
+
+	nonce, _ := hex.DecodeString(s.Nonce)
+	// reverseBytes(nonce)
+
+	header := buildBlockHeader(j.Version, j.PreviousBlockHash, merkleRoot, time, bits, nonce)
+
 	fmt.Println(hex.EncodeToString(merkleRoot))
 
-	time := make([]byte, 4)
-	binary.LittleEndian.PutUint32(time, 1482001679)
-
-	bits := make([]byte, 4)
-	binary.LittleEndian.PutUint32(bits, 402885509)
-
-	nonce := make([]byte, 4)
-	binary.LittleEndian.PutUint32(nonce, 3814348197)
-
-	fmt.Printf("time: %x, bits: %x, nonce: %x\n\n", time, bits, nonce)
-	fmt.Printf("time: %v, bits: %v, nonce: %v\n\n", hex.EncodeToString(time), hex.EncodeToString(bits), hex.EncodeToString(nonce))
-
-	header := buildBlockHeader(536870912, "000000000000000002424db0163641940c9fd999ec897b412ce64e36d6ab7650", merkleRoot, time, bits, nonce)
 	fmt.Printf("%x\nlen=%v\n\n", header, len(header))
 
 	hash := sha256d(header)
